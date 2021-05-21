6daa5632ebfc4538276446f681c0f4a6b0997f17
Mschulen <mateusz.schulen@eengine.pl>
2021-05-21 09:42:12 +0200

    some pointless message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_113751) do
+ActiveRecord::Schema.define(version: 2021_05_21_072813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,15 @@ ActiveRecord::Schema.define(version: 2021_05_18_113751) do
     t.index ["tenant_id", "user_id"], name: "index_tenants_users_on_tenant_id_and_user_id"
   end
 
+  create_table "user_projects", force: :cascade do |t|
+    t.bigint "project_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_user_projects_on_project_id"
+    t.index ["user_id"], name: "index_user_projects_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -85,4 +94,6 @@ ActiveRecord::Schema.define(version: 2021_05_18_113751) do
   add_foreign_key "members", "users"
   add_foreign_key "projects", "tenants"
   add_foreign_key "tenants", "tenants"
+  add_foreign_key "user_projects", "projects"
+  add_foreign_key "user_projects", "users"
 end
